b5103e8f9f61eb58f970681f20754e39c6943741
Aryan Singh <74493183+Aryan6400@users.noreply.github.com>
2024-01-14 02:26:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -75,18 +75,18 @@ io.on("connection", (socket)=>{
 
 // ---------------------- Deployment ---------------------- //
 
-const __dirname1 = path.resolve();
+// const __dirname1 = path.resolve();
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname1, "/frontend/build")));
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-    app.get("*", (req,res)=>{
-        res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
-    })
-}else {
-    app.get('/', (req, res) => {
-        res.send("Hi");
-    })
-}
+//     app.get("*", (req,res)=>{
+//         res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+//     })
+// }else {
+//     app.get('/', (req, res) => {
+//         res.send("Hi");
+//     })
+// }
 
 // ---------------------- Deployment ---------------------- //
